375d22a41ff19d435aed89de3e494db1d1cc56a5
Eiza Stanford <bashsudormstar@gmail.com>
2021-06-22 21:02:38 -0700

    Began work on class Cache_Item and function InitCacheScan; finished function GenerateFileName
    The class Cache_Item is CURRENTLY INCOMPLETE: the idea is to have parts of
    the program that affects/manages ALL cached HTML as global functions,
    while having the data and attributes of the cached HTML and
    object-to-database communication and transactions be handled by the
    attributes and methods of the class. Currently the __init__ constructor
    is somewhat implemented. The function InitCacheScan will scan the database
    file where each line in the file represents a cached html item.

--- fetcher.py
+++ fetcher.py
@@ -4,6 +4,8 @@ ebay scraper 2021 - webpage retrieval and caching system
 
 import requests
 import os
+import random
+import string
 
 
 fileNameList = [
@@ -17,14 +19,52 @@ folderNameList = [
 folderMode = 0o755
 fileMode = 0o644
 
+fileNameGenerateChars = string.ascii_letters + string.digits
+fileNameGenerateLength = 32
+
+# COMPLETE
+def GenerateFileName():
+	return ''.join(random.SystemRandom().choice(fileNameGenerateChars) for loop in range(fileNameGenerateLength))
+
+
+# INCOMPLETE
+class Cache_Item:
+	
+	def __init__(self, url, fileName=None, creationDatetimeObject=None):
+		# If "fileName" is None, automatically generate random filename
+		# If "creationTimestamp" is None, automatically get latest time
+		
+		if not fileName:
+			fileName = GenerateFileName()
+		
+		if not creationDatetimeObject:
+			creationDatetimeObject = datetime.datetime.now()
+		
+		self.fileName = fileName
+		self.creationDatetimeObject = creationDatetimeObject
+
+
+# INCOMPLETE
 def CachePageExpirationCheck(pageUrl):
 	pass
 
 
+# INCOMPLETE
 def WebpageFetch(pageUrl):
 	pass
 
 
+# INCOMPLETE
+def InitCacheScan():
+	# INCOMPLETE
+	with open('fetcher_db.txt', 'r') as fileItself:
+		fileReadLines = fileItself.readlines()
+		
+		for line in fileReadLines:
+			lineSplit = line.split('\t')
+
+
+# COMPLETED "ENOUGH" FOR NOW
 def InitFilesystem():
 	
 	# Create the necessary folders in the folder list.
